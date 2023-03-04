dedce5535d1f651fc876c503d93f96e0fd95b04f
Hugh Shin <hughbee135@gmail.com>
2023-03-04 11:22:19 -0800

    SongList model implemented

--- src/main/java/org/dev/toptenplaylist/model/SongList.java
+++ src/main/java/org/dev/toptenplaylist/model/SongList.java
@@ -1,5 +1,89 @@
 package org.dev.toptenplaylist.model;
 
 public class SongList {
-    // TODO
+    private String id;
+    private String userId;
+    private String title;
+    private long lastModificationDate;
+    private Entry[] entries;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getLastModificationDate() {
+        return lastModificationDate;
+    }
+
+    public void setLastModificationDate(long lastModificationDate) {
+        this.lastModificationDate = lastModificationDate;
+    }
+
+    public Entry[] getEntries() {
+        return entries;
+    }
+
+    public void setEntries(Entry[] entries) {
+        this.entries = new Entry[entries.length];
+        System.arraycopy(entries, 0, this.entries, 0, entries.length);
+    }
+
+    public static class Entry {
+        private String id;
+        private String title;
+        private String artist;
+        private String contentUrl;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getArtist() {
+            return artist;
+        }
+
+        public void setArtist(String artist) {
+            this.artist = artist;
+        }
+
+        public String getContentUrl() {
+            return contentUrl;
+        }
+
+        public void setContentUrl(String contentUrl) {
+            this.contentUrl = contentUrl;
+        }
+    }
 }
