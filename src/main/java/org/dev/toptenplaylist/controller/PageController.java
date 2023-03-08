3b3106280d86e31b327c1a6b324c77d0d17ff711
Hugh Shin <hughbee135@gmail.com>
2023-03-08 12:32:51 -0800

    Implement redirects based on authentication state

--- src/main/java/org/dev/toptenplaylist/controller/PageController.java
+++ src/main/java/org/dev/toptenplaylist/controller/PageController.java
@@ -1,38 +1,62 @@
 package org.dev.toptenplaylist.controller;
 
+import org.dev.toptenplaylist.service.SessionTokenService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class PageController {
+    private final SessionTokenService sessionTokenService;
+
+    public PageController(SessionTokenService sessionTokenService) {
+        this.sessionTokenService = sessionTokenService;
+    }
+
     @GetMapping("/")
-    public String homePage(Model model) {
+    public String homePage(HttpServletResponse response, @CookieValue(value = "session", required = false) String sessionToken, Model model) {
+        sessionTokenService.handleSessionToken(response, sessionToken);
         model.addAttribute("initialPath", "/");
         return "app";
     }
 
     @GetMapping("/login")
-    public String loginPage(Model model) {
+    public String loginPage(HttpServletResponse response, @CookieValue(value = "session", required = false) String sessionToken, Model model) {
+        String activeUserAccountId = sessionTokenService.handleSessionToken(response, sessionToken);
+        if (activeUserAccountId != null) {
+            return "redirect:/";
+        }
         model.addAttribute("initialPath", "/login");
         return "app";
     }
 
     @GetMapping("/signup")
-    public String signUpPage(Model model) {
+    public String signUpPage(HttpServletResponse response, @CookieValue(value = "session", required = false) String sessionToken, Model model) {
+        String activeUserAccountId = sessionTokenService.handleSessionToken(response, sessionToken);
+        if (activeUserAccountId != null) {
+            return "redirect:/";
+        }
         model.addAttribute("initialPath", "/signup");
         return "app";
     }
 
     @GetMapping("/account")
-    public String accountPage(Model model) {
+    public String accountPage(HttpServletResponse response, @CookieValue(value = "session", required = false) String sessionToken, Model model) {
+        String activeUserAccountId = sessionTokenService.handleSessionToken(response, sessionToken);
+        if (activeUserAccountId == null) {
+            return "redirect:/login";
+        }
         model.addAttribute("initialPath", "/account");
         return "app";
     }
 
     @GetMapping("/playlist/{userPublicName}")
-    public String playlistPage(@PathVariable String userPublicName, Model model) {
+    public String playlistPage(HttpServletResponse response, @CookieValue(value = "session", required = false) String sessionToken, @PathVariable String userPublicName, Model model) {
+        sessionTokenService.handleSessionToken(response, sessionToken);
         model.addAttribute("initialPath", "/playlist/" + userPublicName);
         return "app";
     }
